d56cfe9c7d9a2e094407a7ba04e8501afc5f5e56
Nikita Fedoseenkov <nikitaf40@gmail.com>
2020-05-04 16:45:02 +0300

    Переделанный прокси сервер без использования библиотеки javamail.

--- src/main/java/RequestHandler.java
+++ src/main/java/RequestHandler.java
@@ -6,6 +6,8 @@ import java.net.*;
 import java.util.Enumeration;
 import java.util.Properties;
 import javax.mail.*;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
 
 public class RequestHandler implements Runnable {
 
@@ -13,12 +15,14 @@ public class RequestHandler implements Runnable {
 	 * Socket connected to client passed by Proxy server
 	 */
 	Socket clientSocket;
+	SSLSocket toServer;
+	String recvreply;
+
+
 	String username = "null";
 	String password = "null";
 	String email = "null";
 	String host = "null";
-	POP3Store store;
-	POP3Folder inbox;
 	int port = 995;
 	boolean running = true;
 
@@ -46,6 +50,16 @@ public class RequestHandler implements Runnable {
 	 * Send data from proxy to client
 	 */
 	BufferedWriter proxyToClientBw;
+
+	/**
+	 * Read data server sends to proxy
+	 */
+	BufferedReader proxyToServerBr;
+
+	/**
+	 * Send data from proxy to server
+	 */
+	BufferedWriter proxyToServerBw;
 
 
 	/**
@@ -101,22 +115,6 @@ public class RequestHandler implements Runnable {
 				sentStringToClient(capabilities);
 				break;
 			}
-			case "STAT": {
-				statRequest();
-				break;
-			}
-			case "LIST": {
-				listRequest();
-				break;
-			}
-			case "UIDL": {
-				uidlRequest();
-				break;
-			}
-			case "QUIT": {
-				quitRequest();
-				break;
-			}
 			case "AUTH": {
 				sentStringToClient("-ERR" + eol);
 				break;
@@ -125,12 +123,27 @@ public class RequestHandler implements Runnable {
 				sentStringToClient("+OK" + eol);
 				break;
 			}
-			case "RSET": {
-				rsetRequest();
+			case "LIST":
+			case "UIDL": {
+				MultiLineResponse(requestType);
+				break;
+			}
+			case "RSET":
+			case "STAT": {
+				SingleLineResponse(requestType);
+				break;
+			}
+			case "QUIT": {
+				proxyToServerBw.write(requestType + eol);
+				proxyToServerBw.flush();
+				sentStringToClient("+OK" + eol);
+				clientSocket.close();
+				toServer.close();
+				running = false;
 				break;
 			}
 			default:
-				System.out.println("Need to implement simple");
+				System.out.println("NOT IMPLEMENTED");
 				break;
 		}
 	}
@@ -144,78 +157,40 @@ public class RequestHandler implements Runnable {
 			}
 			case "PASS":{
 				setPasswordCredentials(requestLine);
-				connectToEmail();
-				inbox.open(Folder.READ_WRITE);
 				break;
 			}
-			case "LIST":{
-				listComplexRequest(Integer.parseInt(requestLine.substring(requestLine.indexOf(' ') + 1)));
+			case "DELE":
+			case "LIST":
+			case "UIDL": {
+				SingleLineResponse(requestLine);
 				break;
 			}
-			case "UIDL":{
-				uidlComplexRequest(Integer.parseInt(requestLine.substring(requestLine.indexOf(' ') + 1)));
-				break;
-			}
-			case "RETR":{
-				retrComplexRequest(Integer.parseInt(requestLine.substring(requestLine.indexOf(' ') + 1)));
-				break;
-			}
-			case "TOP":{
-				topComplexRequest(Integer.parseInt(requestLine.substring(requestLine.indexOf(' ') + 1, requestLine.indexOf(' ', requestLine.indexOf(' ') + 1))), Integer.parseInt(requestLine.substring(requestLine.indexOf(' ', requestLine.indexOf(' ') + 1) + 1)));
-				break;
-			}
-			case "DELE":{
-				deleComplexRequest(Integer.parseInt(requestLine.substring(requestLine.indexOf(' ') + 1)));
+			case "TOP":
+			case "RETR": {
+				MultiLineResponse(requestLine);
 				break;
 			}
 			default:{
-				System.out.println("Need to implement complex");
+				System.out.println("NOT IMPLEMENTED");
 				break;
 			}
 		}
 	}
 
-	public void rsetRequest() throws MessagingException, IOException {
-		int count = inbox.getMessageCount();
-		Message mail;
-		for (int i = 0; i < count; i++) {
-			mail = inbox.getMessage(i + 1);
-			mail.setFlag(Flags.Flag.DELETED, false);
-		}
-		String answer = "+OK" + eol;
-		sentStringToClient(answer);
+	public void SingleLineResponse(String requestLine) throws IOException {
+		proxyToServerBw.write(requestLine + eol);
+		proxyToServerBw.flush();
+		recvreply = proxyToServerBr.readLine();
+		sentStringToClient(recvreply + eol);
 	}
 
-	public void deleComplexRequest(int number) throws MessagingException, IOException {
-		int count = inbox.getMessageCount();
-		String answer = "";
-		if (number > count) {
-			answer = "-ERR no such message" + eol;
+	public void MultiLineResponse(String requestLine) throws IOException {
+		proxyToServerBw.write(requestLine + eol);
+		proxyToServerBw.flush();
+		while (!(recvreply = proxyToServerBr.readLine()).equals(".")) {
+			sentStringToClient(recvreply + eol);
 		}
-		else {
-			Message mail = inbox.getMessage(number);
-			mail.setFlag(Flags.Flag.DELETED, true);
-			answer = "+OK message " + String.valueOf(number) + " deleted" + eol;
-			sentStringToClient(answer);
-		}
-	}
-
-	public void topComplexRequest(int number, int lines) throws MessagingException {
-		int count = inbox.getMessageCount();
-		String answer = "";
-		if (number > count) {
-			answer = "-ERR no such message" + eol;
-		}
-		else {
-			Message email = inbox.getMessage(number);
-			Enumeration headers = email.getAllHeaders();
-			while (headers.hasMoreElements()) {
-				Header h = (Header) headers.nextElement();
-				answer = answer + h.getName() + ":" + h.getValue() + eol;
-			}
-			answer = "+OK" + eol + answer + "." + eol;
-		}
-		sentStringToClient(answer);
+		sentStringToClient(recvreply + eol);
 	}
 
 	public void getHostFromEmail(){
@@ -238,118 +213,31 @@ public class RequestHandler implements Runnable {
 		}
 	}
 
-	public void connectToEmail() throws MessagingException {
-		Properties props = new Properties();
-		props.setProperty("mail.pop3.ssl.enable", "true");
-		Session session = javax.mail.Session.getInstance(props);
-		store = (POP3Store) session.getStore("pop3");
-		System.out.println("Credentials: " + host + " " + port + " " + username + " " + password);
-		store.connect(host, username, password);
-		System.out.println("Connected " + store.isConnected());
-		inbox = (POP3Folder) store.getFolder("Inbox");
-	}
-
-	public void quitRequest() throws MessagingException, IOException {
-		inbox.close(true);
-		String answer = "+OK" + eol;
-		sentStringToClient(answer);
-		clientSocket.close();
-		running = false;
-	}
-
-	public void statRequest() throws MessagingException {
-		int count = inbox.getMessageCount();
-		int size = inbox.getSize();
-		String answer = "+OK " + String.valueOf(count) + " " + String.valueOf(size) + eol;
-		sentStringToClient(answer);
-	}
-
-	public void retrComplexRequest(int number) throws MessagingException, IOException {
-		int count = inbox.getMessageCount();
-		String answer = "";
-		if (number > count) {
-			answer = "-ERR no such message" + eol;
-		}
-		else {
-			int sizes[] = inbox.getSizes();
-			Message mail = inbox.getMessage(number);
-			answer = "+OK " + String.valueOf(sizes[number - 1]) + " octets" + eol;
-			sentStringToClient(answer);
-			answer = "";
-			mail.writeTo(clientSocket.getOutputStream());
-			sentStringToClient("." + eol);
-		}
-	}
-
-		public void uidlRequest() throws MessagingException {
-		int count = inbox.getMessageCount();
-		String answer = "";
-		if (count == 0) {
-			answer = "-ERR no messages" + eol;
-		}
-		else {
-			for (int i = 0; i < count; i++) {
-				answer = answer + String.valueOf(i + 1) + " " + inbox.getUID(inbox.getMessage(i + 1)) + eol;
-			}
-			answer = "+OK" + eol + answer + "." + eol;
-		}
-		sentStringToClient(answer);
-	}
-
-	public void uidlComplexRequest(int number) throws MessagingException {
-		int count = inbox.getMessageCount();
-		String answer = "";
-		if (number > count) {
-			answer = "-ERR no such message" + eol;
-		}
-		else {
-			answer = "+OK " + String.valueOf(number) + " " + inbox.getUID(inbox.getMessage(number)) + eol;
-		}
-		sentStringToClient(answer);
-	}
-
-	public void listRequest() throws MessagingException {
-		int count = inbox.getMessageCount();
-		int size = inbox.getSize();
-		String answer = "";
-		if (count != 0) {
-			int sizes[] = inbox.getSizes();
-			for (int i = 0; i < count; i++) {
-				answer = answer + String.valueOf(i + 1) + " " + String.valueOf(sizes[i]) + eol;
-			}
-			answer = "+OK " + String.valueOf(count) + " messages (" + String.valueOf(size) + " octets)" + eol + answer + "." + eol;
-		}
-		else {
-			answer = "-ERR no messages" + eol;
-		}
-		sentStringToClient(answer);
-	}
-
-	public void listComplexRequest(int number) throws MessagingException {
-		int count = inbox.getMessageCount();
-		String answer = "";
-		if (number <= count) {
-			int sizes[] = inbox.getSizes();
-			answer = "+OK " + String.valueOf(number) + String.valueOf(sizes[number - 1]) + eol;
-		}
-		else {
-			answer = "-ERR no such message" + eol;
-		}
-		sentStringToClient(answer);
-	}
-
-	public void setUserCredentials(String requestLine){
+	public void setUserCredentials(String requestLine) throws IOException {
 		username = requestLine.substring(requestLine.indexOf(' ') + 1);
 		email = username.substring(username.indexOf("@"));
 		getHostFromEmail();
 		System.out.println("Username is: " + username + "\nEmail server is: " + email);
-		sentStringToClient("+OK password please" + eol);
+		toServer = (SSLSocket) ((SSLSocketFactory) SSLSocketFactory.getDefault()).createSocket(InetAddress.getByName(host), port);
+		toServer.startHandshake();
+
+		proxyToServerBr = new BufferedReader(new InputStreamReader(toServer.getInputStream()));
+		proxyToServerBw = new BufferedWriter(new OutputStreamWriter(toServer.getOutputStream()));
+
+		recvreply = proxyToServerBr.readLine();
+		proxyToServerBw.write(requestLine + eol);
+		proxyToServerBw.flush();
+		recvreply = proxyToServerBr.readLine();
+		sentStringToClient(recvreply + eol);
 	}
 
-	public void setPasswordCredentials(String requestLine){
+	public void setPasswordCredentials(String requestLine) throws IOException {
 		password = requestLine.substring(requestLine.indexOf(' ') + 1);
 		System.out.println("Password is: " + password);
-		sentStringToClient("+OK welcome back" + eol);
+		proxyToServerBw.write(requestLine + eol);
+		proxyToServerBw.flush();
+		recvreply = proxyToServerBr.readLine();
+		sentStringToClient(recvreply + eol);
 	}
 
 	@Override
